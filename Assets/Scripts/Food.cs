f216fee593378e653ca9c98dee7d25f9ae97c4c6
Chase Lewis <lewisc9@wit.edu>
2024-06-16 00:55:46 -0400

    food properties

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,4 +10,12 @@ public class Food : ScriptableObject
     public string foodId;   
     public Sprite foodSprite; // For the visual representation of the food
     public int value;         // The value or amount of the food
+
+    public bool isCookable;
+    public bool isWorkable;
+    public bool isMixable;
+
+    public List<string> mixableFoods; // populated with foodId that is mixable
+
+
 }
